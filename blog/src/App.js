feb2d0411ca1bdbc7cfa79e7fc8cc91395b46aa0
Alexandra Waligorski <waligorski.alexandra@gmail.com>
2023-08-03 11:34:22 +0200

    install swr for data fetching, render blogpost titles

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const {
+    data: blogposts,
+    error,
+    isLoading,
+  } = useSWR("http://localhost:3001/rest/blogposts", fetcher);
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+
+  return (
+    <div className="App">
+      {blogposts.map((post) => (
+        <div>{post.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
