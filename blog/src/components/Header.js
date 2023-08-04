1a1b6306e3bb8b94a0023714d8164cce5df27264
Alexandra Waligorski <waligorski.alexandra@gmail.com>
2023-08-04 12:57:19 +0200

    add back to home navigation to header and styling

--- blog/src/components/Header.js
+++ blog/src/components/Header.js
@@ -1,7 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <header>
       <h3>farbenmeer coding challenge</h3>
+      <NavLink className="header__homelink" to="/">
+        Home
+      </NavLink>
     </header>
   );
 }
